d2bae45c982ed381d99e401da4be2bcb35f25b99
ahmedsoranns <ahmad.soran@netspot.solutions>
2023-03-21 19:08:59 +0300

    changes

--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -10,7 +10,7 @@ import {
   DarkTheme,
 } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import * as React from 'react';
+import React from 'react';
 
 import {
   RootStackParamList,
--- app/utility/helper/Storage.ts
+++ app/utility/helper/Storage.ts
@@ -1,4 +1,3 @@
-import {STORAGE_KEY} from 'constants/App';
 import {MMKV} from 'react-native-mmkv';
 
 export const Storage = new MMKV();
